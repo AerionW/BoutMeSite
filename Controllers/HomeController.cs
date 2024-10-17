201f6ec40496f71640c8480502a5a670c65de09d
Aerion <143963080+AerionW@users.noreply.github.com>
2024-10-17 14:03:05 -0500

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BoutMe.Models;
+
+namespace BoutMe.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.Message = "Sup you soggy cuntwaffle";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult About()
+    {
+        ViewBag.Message = "Bout Me page";
+        return View();
+    }
+}
